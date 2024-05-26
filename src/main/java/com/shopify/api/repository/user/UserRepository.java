794253d54b56a16aac3c442d2e5e9c87fcc4ea38
Sai Nyi Nyi Tun <mgsainyinyitun.tumdy@gmail.com>
2024-05-26 19:10:56 +0630

    login by phone

--- src/main/java/com/shopify/api/repository/user/UserRepository.java
+++ src/main/java/com/shopify/api/repository/user/UserRepository.java
@@ -8,9 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopify.api.models.user.UserEntity;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
-	Optional<UserEntity> findByUsername(String username);
+	
+	@Query( "SELECT u FROM UserEntity u where u.username=:username or u.phone = :username")
+	Optional<UserEntity> findByUsername(@Param("username") String username);
+	
 	UserEntity findByUid(String uid);
 	Boolean existsByUsername(String username);
 
--- src/main/java/com/shopify/api/security/CustomUserDetailsService.java
+++ src/main/java/com/shopify/api/security/CustomUserDetailsService.java
@@ -29,8 +29,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		
 		UserEntity user = userRepository.findByUsername(username)
 				.orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+		
 		return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
 	}
 
